6498bcf7ca7ba9ae65dc052b606090509332c2bb
Munaf Sheikh <munaf.sheikh@gmail.com>
2026-07-18 16:31:03 +0200

    Connect Life Radar to Supabase

--- webpack.common.js
+++ webpack.common.js
@@ -38,6 +38,12 @@ const plugins = [
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
     'process.env.ENABLE_GOOGLE_AUTH': JSON.stringify(process.env.ENABLE_GOOGLE_AUTH),
     'process.env.GTM_ID': JSON.stringify(process.env.GTM_ID),
+    'process.env.SUPABASE_URL': JSON.stringify(
+      process.env.SUPABASE_URL || 'https://zooyilmsocuqannpjhsw.supabase.co'
+    ),
+    'process.env.SUPABASE_PUBLISHABLE_KEY': JSON.stringify(
+      process.env.SUPABASE_PUBLISHABLE_KEY || 'sb_publishable_LnHNJWt-rhyq0xq4hcJh-Q_Nc-nxR5W'
+    ),
   }),
 ]
 
